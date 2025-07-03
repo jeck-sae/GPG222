bb52580cdcd524d0c557bcd24221f6d86ccf8d3a
abdelraiman <abdelrahman.abouzied@student.saedubai.com>
2025-07-03 10:00:58 +0400

    adding server as well as a checker for disconect in the server

---
+++ Assets/Scripts/Networking/Server.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using System.Net;
+using System.Text;
+
+public class Server : MonoBehaviour
+{
+    Socket serverSocket;
+    int port = 3000;
+
+    List<Socket> clientsSocket;
+
+    void Start()
+    {
+        clientsSocket = new List<Socket>();
+
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+        serverSocket.Listen(5);
+        serverSocket.Blocking = false;
+    }
+
+    void Update()
+    {
+        try
+        {
+            clientsSocket.Add(serverSocket.Accept());
+            Debug.LogError("Client connected: ");
+        }
+        catch
+        {
+        }
+
+        for (int i = 0; i < clientsSocket.Count; i++)
+        {
+            if (clientsSocket[i].Poll(0, SelectMode.SelectRead) && clientsSocket[i].Available == 0)
+            {
+                Debug.Log("Client disconnected.");
+                clientsSocket[i].Close();
+                clientsSocket.RemoveAt(i);
+                i--;
+                continue;
+            }
+        }
+
+        try
+        {
+            for (int i = 0; i < clientsSocket.Count; i++)
+            {
+                if (clientsSocket[i].Available > 0)
+                {
+                    byte[] buffer = new byte[clientsSocket[i].Available];
+                    clientsSocket[i].Receive(buffer);
+
+                    for (int j = 0; j < clientsSocket.Count; j++)
+                    {
+                        if (j != i)
+                        {
+                            clientsSocket[j].Send(buffer);
+                        }
+                    }
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
+}

d74444a47633a406dce3f3a23b35de493f457991
MTACreations <mohammad.almheiri@student.saedubai.com>
2025-08-02 15:28:17 +0400

    Level Loader

---
+++ Assets/Scripts/Game/SceneLoading.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Networking;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        NetworkEvents.LoadLevelPacketReceived += LoadLevel;
+    }
+
+    private void LoadLevel(LoadLevelPacket packet)
+    {
+        SceneManager.LoadScene(packet.levelId);
+    }
+
+    private void OnDisable()
+    {
+        NetworkEvents.LoadLevelPacketReceived -= LoadLevel;
+    }
+}
